278f0e35f652490da2c53b36334f124156e50584
a.popov <popalex@gmail.com>
2016-11-17 15:10:06 +0300

    Initial commit

---
+++ PyYaSearch.py
@@ -0,0 +1,32 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        assert "No results found." not in driver.page_source
+        elem.send_keys(Keys.RETURN)
+
+    def test_search_in_ya_ru(self):
+        driver = self.driver
+        driver.get("http://www.ya.ru")
+        self.assertIn("Яндекс", driver.title)
+        elem = driver.find_element_by_name("text")
+        elem.send_keys("yandex")
+        assert "No results found." not in driver.page_source
+        elem.send_keys(Keys.RETURN)
+
+    #def tearDown(self):
+        #self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
